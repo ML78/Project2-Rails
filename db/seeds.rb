137303070f540b7b5bdd89fb67799f443bad484b
Monique Lee <moniquelee.7878@gmail.com>
2018-04-21 17:08:10 +1000

    update controllers, views, user model, seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
 User.destroy_all
 u1 = User.create(:image => 'https://i.imgur.com/vedQYVM.jpg', :name => 'Admin', :email => 'admin@gmail.com', :location => 'Sydney', :password => 'admin', :admin => true)
 u2 = User.create(:image => 'https://i.imgur.com/3ukJaY7.jpg', :name => 'Julie', :email => 'julie@gmail.com', :location => 'Sydney', :password => 'julie', :admin => false)
-u3 = User.create(:image => 'https://i.imgur.com/d4ZN1sS.jpg', :name => 'Monique', :email => 'monique@gmail.com', :location => 'Sydney', :password => 'monique', :admin => false)
-
-
+u3 = User.create(:image => 'https://i.imgur.com/d4ZN1sS.jpg?1', :name => 'Monique', :email => 'monique@gmail.com', :location => 'Sydney', :password => 'monique', :admin => false)
 
 Task.destroy_all
 t1 = Task.create(:name => 'Reusable Bottle', :description => 'Refill your reusable bottle', :category => 'Food & Drink', :score => 2)
@@ -13,6 +11,7 @@ t4 = Task.create(:name => 'Brush smart', :description => 'Turn off the water whe
 t5 = Task.create(:name => 'Carpooling', :description => 'Share your next ride', :category => 'Transport', :score => 2)
 
 
-Achievement.destroy_all
-a1 = Achievement.create(:user_id => 3, :task_id => 3)
-a2 = Achievement.create(:user_id => 4, :task_id => 4)
+u2.tasks << t1
+u2.tasks << t2
+u3.tasks << t3
+u3.tasks << t4
